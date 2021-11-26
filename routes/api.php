b165b67294e9f8ed675d1a09e00dccba5f4e3f09
Drago Stevanovic <drago@sounder.fm>
2021-11-26 18:58:56 +0100

    JWT Authentication

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\MovieController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -15,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/movies', [MovieController::class, 'index']);
+Route::get('/movies', [MovieController::class, 'index'])->middleware('auth:api');
 Route::get('/movies/{movie}', [MovieController::class, 'show']);
 Route::post('/movies', [MovieController::class, 'store']);
 Route::put('/movies/{movie}', [MovieController::class, 'update']);
@@ -24,3 +25,11 @@ Route::delete('/movies/{movie}', [MovieController::class, 'destroy']);
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::post('/auth/login', [AuthController::class, 'login']);
+Route::get('/auth/me', [AuthController::class, 'getActiveUser'])->middleware('auth:api');
+Route::post('/auth/register', [AuthController::class, 'register']);
+Route::post('/auth/logout', [AuthController::class, 'logout'])->middleware('auth:api');
+
+Route::post('/auth/refresh', [AuthController::class, 'refreshToken']);
